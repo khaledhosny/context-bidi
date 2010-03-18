39d12a315a0a899f5bdbb5f63bbc61a89cb104bc
Khaled Hosny <khaledhosny@eglug.org>
2010-03-18 16:24:22 +0200

    Make tests more verbose

--- biditest.lua
+++ biditest.lua
@@ -4,10 +4,22 @@ require("bidi.lua")
 
 file = io.open("minitests.txt", "r")
 
+s, f = 0, 0
 function dodobidi(str)
-	print(str)
-	doBidi(str)
-	os.execute(string.format("echo '%s' | fribidi --caprtl --levels --novisual", str))
+	local result1 = doBidi(str)
+	local cmd     = string.format("echo '%s' | fribidi --caprtl --levels --novisual", str)
+	local fribidi = io.popen(cmd, 'r')
+	local result2 = fribidi:read('*a'):gsub("\n","")
+	if result1 == result2 then
+--		print(str)
+--		print("sucess")
+		s = s + 1
+	else
+		print(str)
+		print(result1)
+		print(result2)
+		f = f + 1
+	end
 end
 
 function main()
@@ -16,6 +28,7 @@ function main()
 --		print(t[1], t[2])
 		dodobidi(t[1])
 	end
+	print(s.." sucesses,"..f.." failures.")
 end
 --dodobidi("A TEST FOR WEAK TYPES. 123+,456")
 main()
